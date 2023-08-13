71abcf6becdb8fd32032265d3efdcb4f086f2141
Cassiana <lima.cglima@gmail.com>
2023-08-13 14:13:36 -0300

    dia 05

---
+++ dia05/pedido_item_preco.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM item_pedido AS t1
+    LEFT JOIN produto AS t2 ON t1.descItem = t2.descItem
+WHERE descTipoItem like '%massa%'
---
+++ dia05/qtde_pedidos_estado.sql
@@ -0,0 +1,4 @@
+SELECT descUF,
+    count(distinct idPedido) as qtdePedido
+FROM pedido
+GROUP BY descUF
